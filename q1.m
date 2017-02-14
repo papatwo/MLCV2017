a82ae0ad87c3c9bfd21c8e7e795f361ca7c39f10
papatwo <byw94@hotmail.com>
2017-02-14 22:52:31 +0000

    branch test

--- q1.m
+++ q1.m
@@ -10,5 +10,5 @@ end
 
 % bags with replacement of 63.2% unique data
 frac = 1 - 1/exp(1); % Bootstrap sampling fraction: 1 - 1/e (63.2%)
-[N,D]=size(data_train);
-idx = randsample(N,ceil(N*frac),1); 
+[N,D]=size(data_train); 
+idx = randsample(N,ceil(N*frac),1); % create one subbag
