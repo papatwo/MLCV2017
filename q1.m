3f9e6786aef92bfad479f736f34e2fa6ebf9c73d
papatwo <byw94@hotmail.com>
2017-02-14 22:16:03 +0000

    add provided doc

---
+++ q1.m
@@ -0,0 +1,14 @@
+init;
+[data_train, data_test] = getData('Toy_Spiral'); 
+
+%% to create  bags of dataset 
+% 4 bags without replacement
+group = crossvalind('Kfold', length(data_train(:,1)), 4);
+for i=1:4
+    bag_re{i} = data_train(group==i,:); 
+end
+
+% bags with replacement of 63.2% unique data
+frac = 1 - 1/exp(1); % Bootstrap sampling fraction: 1 - 1/e (63.2%)
+[N,D]=size(data_train);
+idx = randsample(N,ceil(N*frac),1); 
