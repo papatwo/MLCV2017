f394a8411c61827e1be98d8b9a70fb15f41c29f6
papatwo <byw94@hotmail.com>
2017-02-15 13:32:02 +0000

    bags

--- q1.m
+++ q1.m
@@ -3,13 +3,15 @@ init;
 
 %% to create  bags of dataset 
 % 4 bags without replacement
-group = crossvalind('Kfold', length(data_train(:,1)), 4);
-for i=1:4
-    bag_re{i} = data_train(group==i,:); 
-end
+% group = crossvalind('Kfold', length(data_train(:,1)), 4);
+% for i=1:4
+%     bag_uni{i} = data_train(group==i,:); 
+% end
 
 % bags with replacement of 63.2% unique data
 frac = 1 - 1/exp(1); % Bootstrap sampling fraction: 1 - 1/e (63.2%)
-[N,D]=size(data_train); 
-idx = randsample(N,ceil(N*frac),1); %test multi
-%hello
+[N,D]=size(data_train);
+bag={};
+for i=1:4
+    bag{i} = randsample(N,ceil(N*frac),1); % 4 bags of samples with replacement  
+end
