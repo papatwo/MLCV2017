ce7ff602d3c9d9cb95ba04f4208c8bb3b048c7e6
papatwo <byw94@hotmail.com>
2017-04-06 17:57:17 +0100

    compute homography matrix with two methods. SVD result might be wrong

---
+++ cw2/get_homography.m
@@ -0,0 +1,27 @@
+function H = get_homography(corre1,corre2)
+% input points vector must be 2xN size and at least contain 4 points
+
+% x1 = corre1(1,:);
+% y1 = corre1(2,:);
+% x2 = corre2(1,:);
+% y2 = corre2(2,:);
+n = size(corre1,2);
+A = zeros(2*n,9);
+
+for i = 1:n % for loop to create A matrix where Ah=0
+    A(2*i-1,:) = [corre1(:,i)',1,0,0,0,-corre1(:,i)'.*corre2(1,i),-corre2(1,i) ];
+    A(2*i,:) = [0,0,0,corre1(:,i)',1,-corre1(:,i)'.*corre2(2,i),-corre2(2,i) ];
+end
+
+if n==4 % there are right no. equations to solve 8 unknows in H
+    H = null(A);
+    H = reshape(H(:,end),[3,3]);
+elseif n>4
+    [~,~,V] = svd(A);
+    H = reshape(V(:,end),[3 3])';
+else
+    error('Not enough points');
+end
+H = H./H(end,end);
+end
+
--- cw2/main.m
+++ cw2/main.m
@@ -1,11 +1,15 @@
 %% Q1. Matching
 % close all; clear all
 % 1) Manual
-
+img = im2double(imread('images.jpg'));
+% img = im2double(imread('test1.png'));
+% img = im2double(imread('test2.png'));
+imshow(img);
+[X1,Y1] = ginput(5); % get interest points in imgA
+[X2,Y2] = ginput(5); % get interest points in imgB
 %% 2) Automatic
 % a) Harris points detector
-
-%%
+%
 % b) calculate local feature descriptor from interest points
 
 % Get image gradient magnitudes and directions at each pixel
@@ -19,10 +23,7 @@
 % cell. Concatente and normalize to get features for interest point
 
 %  Return descriptors for each interest point
-
-
-
-%% 
+%
 % c) NN matching between descriptors from two different images
 
 % For each interest point, get ratio of distance of the nearest neighbor to
@@ -74,3 +75,46 @@ indexPairs = matchFeatures(features1,features2);
 matchedPoints1 = valid_points1(indexPairs(:,1),:);
 matchedPoints2 = valid_points2(indexPairs(:,2),:);
 figure; showMatchedFeatures(I1,I2,matchedPoints1,matchedPoints2);
+
+%% 3) Transformation estimation
+
+% a) homography matrix
+
+% Assume we have two images '1' and '2' and four feature pairs:
+% (p1 is the reference image)
+% corre1 is a 2*n matrix. Each row is the coordinates of a point in '1'
+% corre2 is a 2*n matrix. Each row is the coordinates of a point in '2'
+% H is the homography matrix, such that:
+% corre1_homogeneous = H * [corre2 ones(size(corre2, 1), 1)]'
+n = size(corre1, 2);
+if n < 3
+ error('Not enough points');
+end 
+
+% the original transformation formula is:
+% img2 = h*img1 where H is 3x3 matrix
+% for the convenience of calculation expand H into col vector and reshape X
+% after reshape and expand : x = X*H where H is 9x1 vector
+
+X = zeros(n*3,9); % reshape of img1 denoted as X
+x = zeros(n*3,1); % corresponding pt in img2 denoted as x
+for i=1:n 
+ X(3*(i-1)+1,1:3) = [corre2(:,i)',1];
+ X(3*(i-1)+2,4:6) = [corre2(:,i)',1];
+ X(3*(i-1)+3,7:9) = [corre2(:,i)',1];
+ x(3*(i-1)+1:3*(i-1)+3) = [corre1(:,i);1];
+end
+H = (X\x)'; % obtained from x = X*H
+H = reshape(H,3,3)';
+% for transformation test:
+i = 1; % change this number to any point you want to test
+corre = H*[corre1(:,i);1];
+
+%% by using SVD solution to compute H
+% cannot get corresponding point when apply img2 = H*img1
+H = get_homography(corre1,corre2);
+% for transformation test:
+i = 1; % change this number to any point you want to test
+corre = H*[corre1(:,i);1];
+
+
